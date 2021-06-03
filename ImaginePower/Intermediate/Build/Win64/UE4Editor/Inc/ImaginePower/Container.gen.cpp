acdc1a8f9b9c10d144b7661d99ae948b5abf8ff3
proti1 <protizero@gmail.com>
2021-06-03 15:06:12 +0200

    Dodanie folderów i klas: Container i Minion
    Utworzenie folderów w kodzie źródłowym: Characters, Items i Base
    Dodanie Minion do Characters
    Dodanie Container do Items
    Utwodzenie folderów w Content: Characters, Items, Container

---
+++ ImaginePower/Intermediate/Build/Win64/UE4Editor/Inc/ImaginePower/Container.gen.cpp
@@ -0,0 +1,84 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "ImaginePower/Items/Container.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeContainer() {}
+// Cross Module References
+	IMAGINEPOWER_API UClass* Z_Construct_UClass_AContainer_NoRegister();
+	IMAGINEPOWER_API UClass* Z_Construct_UClass_AContainer();
+	ENGINE_API UClass* Z_Construct_UClass_AActor();
+	UPackage* Z_Construct_UPackage__Script_ImaginePower();
+// End Cross Module References
+	void AContainer::StaticRegisterNativesAContainer()
+	{
+	}
+	UClass* Z_Construct_UClass_AContainer_NoRegister()
+	{
+		return AContainer::StaticClass();
+	}
+	struct Z_Construct_UClass_AContainer_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_AContainer_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_AActor,
+		(UObject* (*)())Z_Construct_UPackage__Script_ImaginePower,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AContainer_Statics::Class_MetaDataParams[] = {
+		{ "IncludePath", "Items/Container.h" },
+		{ "ModuleRelativePath", "Items/Container.h" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_AContainer_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AContainer>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_AContainer_Statics::ClassParams = {
+		&AContainer::StaticClass,
+		"Engine",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x009000A4u,
+		METADATA_PARAMS(Z_Construct_UClass_AContainer_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_AContainer_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_AContainer()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_AContainer_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AContainer, 3048892523);
+	template<> IMAGINEPOWER_API UClass* StaticClass<AContainer>()
+	{
+		return AContainer::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AContainer(Z_Construct_UClass_AContainer, &AContainer::StaticClass, TEXT("/Script/ImaginePower"), TEXT("AContainer"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AContainer);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ ImaginePower/Intermediate/Build/Win64/UE4Editor/Inc/ImaginePower/Minion.gen.cpp
@@ -0,0 +1,85 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "ImaginePower/Characters/Minion.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeMinion() {}
+// Cross Module References
+	IMAGINEPOWER_API UClass* Z_Construct_UClass_AMinion_NoRegister();
+	IMAGINEPOWER_API UClass* Z_Construct_UClass_AMinion();
+	ENGINE_API UClass* Z_Construct_UClass_ACharacter();
+	UPackage* Z_Construct_UPackage__Script_ImaginePower();
+// End Cross Module References
+	void AMinion::StaticRegisterNativesAMinion()
+	{
+	}
+	UClass* Z_Construct_UClass_AMinion_NoRegister()
+	{
+		return AMinion::StaticClass();
+	}
+	struct Z_Construct_UClass_AMinion_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_AMinion_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_ACharacter,
+		(UObject* (*)())Z_Construct_UPackage__Script_ImaginePower,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AMinion_Statics::Class_MetaDataParams[] = {
+		{ "HideCategories", "Navigation" },
+		{ "IncludePath", "Characters/Minion.h" },
+		{ "ModuleRelativePath", "Characters/Minion.h" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_AMinion_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AMinion>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_AMinion_Statics::ClassParams = {
+		&AMinion::StaticClass,
+		"Game",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x009000A4u,
+		METADATA_PARAMS(Z_Construct_UClass_AMinion_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_AMinion_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_AMinion()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_AMinion_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AMinion, 2149107019);
+	template<> IMAGINEPOWER_API UClass* StaticClass<AMinion>()
+	{
+		return AMinion::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AMinion(Z_Construct_UClass_AMinion, &AMinion::StaticClass, TEXT("/Script/ImaginePower"), TEXT("AMinion"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AMinion);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
